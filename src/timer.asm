65b6c1555b7425cb18e9c1a91aea0ac15cbead70
Dalton Claybrook <daltonclaybrook@gmail.com>
2019-10-12 23:33:24 -0400

    Now changing direction of snake

--- src/timer.asm
+++ src/timer.asm
@@ -1,7 +1,7 @@
 section "Timer", rom0
 
 ; approx 16 ticks per second
-TIMER_COUNT EQU 10 ; Count of timer fires until advancing gameplay
+TIMER_COUNT EQU 4 ; Count of timer fires until advancing gameplay
 
 Timer::
     push af
@@ -31,8 +31,8 @@ GameTick::
     ld [wHeadOffset], a
     inc a
     call LoadLocationAtOffsetIntoBC
+    call UpdateBCLocationBasedOnDirection
     ld a, [wHeadOffset]
-    inc bc
     call LoadLocationBCIntoOffset
     ret
 
@@ -61,4 +61,43 @@ LoadLocationBCIntoOffset::
     ld [hli], a
     ld a, c
     ld [hl], a
+    ret
+
+; @param `bc` - location to update
+UpdateBCLocationBasedOnDirection::
+    ld a, [hDirection]
+    ld h, b
+    ld l, c ; `hl` contains the location to update
+    ld de, $20 ; `de` contains the length of one full row of tiles in the bg map
+.checkRight
+    cp DIRECTION_RIGHT
+    jr nz, .checkDown
+    inc bc
+    ret
+.checkDown
+    cp DIRECTION_DOWN
+    jr nz, .checkLeft
+    add hl, de
+    ld b, h
+    ld c, l
+    ret
+.checkLeft
+    cp DIRECTION_LEFT
+    jr nz, .checkUp
+    dec bc
+    ret
+.checkUp
+    cp DIRECTION_UP
+    jr nz, .finish
+    ld a, l
+    sub e
+    ld l, a
+    jr nc, .noCarry
+    ld a, h
+    sub 1
+    ld h, a
+.noCarry
+    ld b, h
+    ld c, l
+.finish
     ret
